b3137b443e6b519f491515dcd9d062e41ba59cc6
LeonardsonCC <leonardson.carvalho@gmail.com>
2026-01-30 12:02:39 -0300

    refactor: clean up imports and add command aliases in ShopCommands

--- src/main/java/br/com/leonardson/taleshop/shop/command/ShopCommands.java
+++ src/main/java/br/com/leonardson/taleshop/shop/command/ShopCommands.java
@@ -1,16 +1,17 @@
 package br.com.leonardson.taleshop.shop.command;
 
+import com.hypixel.hytale.server.core.command.system.basecommands.AbstractCommandCollection;
+
 import br.com.leonardson.taleshop.shop.ShopRegistry;
 import br.com.leonardson.taleshop.shop.trade.command.TradeCommands;
-
-import com.hypixel.hytale.protocol.GameMode;
-import com.hypixel.hytale.server.core.command.system.basecommands.AbstractCommandCollection;
 
 public class ShopCommands extends AbstractCommandCollection {
     public ShopCommands(ShopRegistry shopRegistry) {
         super("shop", "Shop commands");
 
         this.requirePermission("taleshop.shop.manage");
+
+        this.addAliases("taleshop", "tshop");
 
         addSubCommand(new CreateShopCommand(shopRegistry));
         addSubCommand(new RenameShopCommand(shopRegistry));
